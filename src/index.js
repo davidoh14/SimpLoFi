8c09c4155e3adc56d9c00a61aa20f54acc814f95
David Oh <davidohyoung@gmail.com>
2021-09-04 23:07:26 -0500

    built keyboard virtual keyboard and functionality to play notes upon clicks and key presses

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const keyboard = ['1', '2', '3','4', '5', '6', '7', '8', '9', '0', '-', 
+'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 
+'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
+'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/']
+
+const keys = document.querySelectorAll('.key')
+
+
+keys.forEach(key => {
+    key.addEventListener('click', () => playNote(key))
+    key.addEventListener('onkeydown', () => playNote(key))
+})
+
+
+document.addEventListener('keydown', e => {
+    if (e.repeat) return
+    const key = e.key
+    const keyIndex = keyboard.indexOf(key)
+
+    if (keyIndex > -1) playNote(keys[keyIndex])
+})
+
+function playNote(key){
+    const noteAudio = document.getElementById(key.dataset.note)
+    noteAudio.currentTime = 0
+    noteAudio.play()
+    key.classList.add('active')
+    noteAudio.addEventListener('ended', () => {
+        key.classList.remove('active')
+    })
+}
+
+
+
